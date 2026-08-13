a787761a505cd67cd8b4965cce5582c2e470df13
alan <alancosta1988@gmail.com>
2026-08-13 17:01:16 -0300

    130826

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ filename = '260805/shafir1/'
 
 inicio1 = time.perf_counter()
 
-p.params['nx'] = int(2**12)
+p.params['nx'] = int(2**13)
 p.params['nx'] += 1
 p.params['xn'] = 30*2
-# p.params['f'] = 0.05
-# p.params['v'] = 0.1
-# p.params['c_salt'] = 3.01e-7
-# p.params['w2'] = 1500
-# p.params['phib2']=1e-6
+p.params['f'] = 0.05
+p.params['v'] = 0.1
+p.params['c_salt'] = 3.01e-7
+p.params['w2'] = 1500
+p.params['phib2']=1e-6
 p.params['relaxation_tax'] = 7e-3
 p.params['error'] = 1e-4
-# p.params['dirichlet_boundary'] = [False, True, False, True]
+p.params['dirichlet_boundary'] = [False, True, False, True]
 # p.params['y0'] = -4*np.pi*p.params['lb']*1e-4*30
-# p.params['y0'] = -0.01
+p.params['y0'] = -0.01
 # p.params['yn'] = 0.0
 
 t = ''
